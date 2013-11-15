a4e16478c70342da147fd615602134053e144d23
老牛啊 <obullxl@gmail.com>
2013-11-15 19:10:47 +0800

    add config Signed-off-by: 老牛啊 <obullxl@gmail.com>

---
+++ src/main/java/com/github/obullxl/lang/config/ConfigFactory.java
@@ -0,0 +1,202 @@
+/**
+ * Author: obullxl@gmail.com
+ * Copyright (c) 2004-2013 All Rights Reserved.
+ */
+package com.github.obullxl.lang.config;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.URL;
+import java.net.UnknownHostException;
+import java.util.Arrays;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.Set;
+
+import org.apache.commons.lang.StringUtils;
+
+/**
+ * 配置工厂
+ * 
+ * @author obullxl@gmail.com
+ * @version $Id: ConfigFactory.java, V1.0.1 2013年11月15日 下午4:18:00 $
+ */
+public class ConfigFactory {
+    private static Configration       config;
+
+    private static String             DEFAULT_HOST_NAME = "app";
+
+    private static final List<String> paths             = Arrays.asList("config-test.xml", "config/config-test.xml", "config-test.properties", "config/config-test.properties", "config.xml",
+                                                            "config/config.xml", "config.properties", "config/config.properties");
+
+    /**
+     * 取得Configration对象
+     */
+    public static Configration getConfigration() {
+        if (config == null) {
+            config = new Configration();
+
+            //从配置文件中加载配置
+            loadFromConfig();
+
+            //从系统属性中加载配置
+            loadFromSystem();
+
+            // 打印系统参数
+            print();
+        }
+
+        return config;
+    }
+
+    /**
+     * 获取配置文件
+     */
+    private static String findConfigPath() {
+        ClassLoader clsloader = Thread.currentThread().getContextClassLoader();
+        for (String path : paths) {
+            URL url = clsloader.getResource(path);
+            if (url != null) {
+                return path;
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * 从配置文件中加载配置
+     */
+    private static void loadFromConfig() {
+        String path = findConfigPath();
+        if (StringUtils.isBlank(path)) {
+            throw new RuntimeException("配置文件不存在-" + paths);
+        }
+
+        ClassLoader clsloader = Thread.currentThread().getContextClassLoader();
+        URL url = clsloader.getResource(path);
+
+        Properties properties = new Properties();
+        try {
+            if (StringUtils.endsWithIgnoreCase(path, ".xml")) {
+                properties.loadFromXML(url.openStream());
+            } else {
+                properties.load(url.openStream());
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("无法加载配置文件-" + path);
+        }
+
+        for (Map.Entry<Object, Object> entry : properties.entrySet()) {
+            String key = StringUtils.trimToEmpty(entry.getKey().toString());
+            String value = StringUtils.trimToEmpty(entry.getValue().toString());
+
+            // 从系统参数中取值，如果系统参数中有值，系统参数的值生效
+            String sysValue = System.getProperty(key);
+            if (StringUtils.isNotEmpty(sysValue)) {
+                System.out.println("在Java -D参数中发现key[" + key + "]，将使用系统参数设置的值[" + sysValue + "] 替换原有的值 [" + value + "]");
+                value = sysValue;
+            }
+
+            config.setProperty(key, value);
+        }
+
+    }
+
+    /**
+     * 从系统属性中加载配置
+     * 
+     * <p>
+     * 从系统属性中读取配置信息，不是所有的配置都是有用的，所以没有必要都做处理
+     * </p>
+     */
+    private static void loadFromSystem() {
+        // 设置本地的host名称
+        config.setProperty(Configration.SYS_HOST_NAME, getHostName());
+
+        // 设置本机IP
+        config.setProperty(Configration.SYS_IP, getNetworkAddress());
+
+        // 校验本机的应用名称
+        String sysAppName = config.getPropertyValue(Configration.SYS_APP_NAME);
+        if (sysAppName == null || sysAppName.length() == 0) {
+            throw new RuntimeException("必须设置app_name属性");
+        }
+    }
+
+    private static void print() {
+        Map<String, String> configs = config.getConfig();
+        Set<Map.Entry<String, String>> configSet = configs.entrySet();
+
+        System.out.println("系统启动参数:");
+        for (Iterator<Map.Entry<String, String>> iterator = configSet.iterator(); iterator.hasNext();) {
+            Map.Entry<String, String> entry = iterator.next();
+
+            System.out.println(" | " + entry.getKey() + "  [" + entry.getValue() + "]");
+        }
+    }
+
+    /**
+     * 在超过一块网卡时有点问题，因为这里每次都只是取了第一块网卡绑定的IP地址
+     */
+    private static String getNetworkAddress() {
+        Enumeration<NetworkInterface> netInterfaces;
+        try {
+            netInterfaces = NetworkInterface.getNetworkInterfaces();
+            InetAddress ip = null;
+            while (netInterfaces.hasMoreElements()) {
+                NetworkInterface ni = (NetworkInterface) netInterfaces.nextElement();
+                Enumeration<InetAddress> addresses = ni.getInetAddresses();
+                while (addresses.hasMoreElements()) {
+                    ip = (InetAddress) addresses.nextElement();
+                    if (!ip.isLoopbackAddress() && ip.getHostAddress().indexOf(":") == -1) {
+                        return ip.getHostAddress();
+                    } else {
+                        continue;
+                    }
+                }
+            }
+            return "";
+        } catch (Exception e) {
+            return "";
+        }
+    }
+
+    /**
+     * 取得主机名称
+     */
+    private static String getHostName() {
+        // 设置本地的host名称
+        String hostName = DEFAULT_HOST_NAME;
+        try {
+            hostName = InetAddress.getLocalHost().getHostName();
+        } catch (UnknownHostException e) {
+            //ignore
+            hostName = DEFAULT_HOST_NAME;
+        }
+        return hostName;
+    }
+
+    public static void main(String[] args) {
+        String hostName = DEFAULT_HOST_NAME;
+        String ip = null;
+        try {
+            hostName = InetAddress.getLocalHost().getHostName();
+
+            ip = getNetworkAddress();
+        } catch (UnknownHostException e) {
+            //ignore
+            e.printStackTrace();
+        }
+
+        System.out.println("ip :" + ip);
+        System.out.println("hostname :" + hostName);
+        
+        System.out.println();
+        getConfigration();
+    }
+
+}
---
+++ src/main/java/com/github/obullxl/lang/config/Configration.java
@@ -0,0 +1,102 @@
+/**
+ * Author: obullxl@gmail.com
+ * Copyright (c) 2004-2013 All Rights Reserved.
+ */
+package com.github.obullxl.lang.config;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * 配置类
+ * 
+ * @author obullxl@gmail.com
+ * @version $Id: Configration.java, V1.0.1 2013年11月15日 下午4:18:52 $
+ */
+public class Configration {
+
+    /** 系统唯一标识符 */
+    public static final String  SYS_IDENTIFY  = "identify";
+
+    /** 本地主机名称 */
+    public static final String  SYS_HOST_NAME = "sys_host_name";
+    /** 本机ip */
+    public static final String  SYS_IP        = "sys_ip";
+    /** 应用名称 */
+    public static final String  SYS_APP_NAME  = "app_name";
+    /** 运行模式 */
+    public static final String  SYS_RUN_MODE  = "run_mode";
+
+    public static final String  SYS_VERSION   = "sys_version";
+
+    public static final String  LOG_CONFIG    = "log_config";
+
+    /** key 常量 */
+    public static final String  LOG4J_PATH    = "log4j_path";
+
+    // ~~~~ 内部变量
+
+    private Map<String, String> configs       = new LinkedHashMap<String, String>();
+
+    // ~~~~  构造函数
+
+    protected Configration() {
+    }
+
+    public Map<String, String> getConfig() {
+        //对map进行clone，保证map的安全性
+        Map<String, String> map = new HashMap<String, String>();
+        map.putAll(this.configs);
+        return map;
+    }
+
+    /**
+     * 设置配置信息
+     */
+    public Configration setConfigs(Map<String, String> map) {
+        this.configs.putAll(map);
+        return this;
+    }
+
+    public String getPropertyValue(String key) {
+        return this.configs.get(key);
+    }
+
+    /**
+     * 设置配置属性
+     */
+    public Configration setProperty(String key, String value) {
+        this.configs.put(key, value);
+        return this;
+    }
+
+    public String getPropertyValue(String key, String defaultValue) {
+        return this.configs.get(key) == null ? defaultValue : this.configs.get(key);
+    }
+
+    public String getSysAppName() {
+        return getPropertyValue(SYS_APP_NAME);
+    }
+
+    public String getSysIp() {
+        return getPropertyValue(SYS_IP);
+    }
+
+    public String getSysRunMode() {
+        return getPropertyValue(SYS_RUN_MODE);
+    }
+
+    public String getSysHostName() {
+        return getPropertyValue(SYS_HOST_NAME);
+    }
+
+    public String getSysVersion() {
+        return getPropertyValue(SYS_VERSION);
+    }
+
+    public String getLogConfig() {
+        return getPropertyValue(LOG_CONFIG);
+    }
+
+}
