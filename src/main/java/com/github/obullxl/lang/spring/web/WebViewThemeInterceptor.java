0b0bc5f3cbde03b703ba838067b9342016c00fa4
obullxl <obullxl@163.com>
2014-01-12 19:49:13 +0800

    update theme Signed-off-by: obullxl <obullxl@163.com>

--- src/main/java/com/github/obullxl/lang/spring/web/WebViewThemeInterceptor.java
+++ src/main/java/com/github/obullxl/lang/spring/web/WebViewThemeInterceptor.java
@@ -4,9 +4,13 @@
  */
 package com.github.obullxl.lang.spring.web;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
@@ -19,17 +23,27 @@ import com.github.obullxl.lang.utils.LogUtils;
  * @version $Id: WebViewThemeInterceptor.java, 2012-8-11 下午3:29:35 Exp $
  */
 public class WebViewThemeInterceptor extends HandlerInterceptorAdapter {
-    private static final Logger logger = LogUtils.get();
+    private static final Logger logger           = LogUtils.get();
+
+    /** 忽略URI前缀 */
+    private Set<String>         ignoreUriPrefixs = new HashSet<String>();
 
     /**
      * @see org.springframework.web.servlet.handler.HandlerInterceptorAdapter#preHandle(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse, java.lang.Object)
      */
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        String url = request.getRequestURI();
+        for (String prefix : this.ignoreUriPrefixs) {
+            if (StringUtils.startsWith(url, prefix)) {
+                return true;
+            }
+        }
+
         String theme = "bootword";
         WebViewThemeHolder.set(theme);
 
-        if (logger.isInfoEnabled()) {
-            logger.info("[视图主题拦截器]-设置主题[" + WebViewThemeHolder.get() + "].");
+        if (logger.isDebugEnabled()) {
+            logger.debug("[视图主题拦截器]-设置主题[" + WebViewThemeHolder.get() + "].");
         }
 
         return true;
@@ -39,11 +53,17 @@ public class WebViewThemeInterceptor extends HandlerInterceptorAdapter {
      * @see org.springframework.web.servlet.handler.HandlerInterceptorAdapter#afterCompletion(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse, java.lang.Object, java.lang.Exception)
      */
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception e) {
-        if (logger.isInfoEnabled()) {
-            logger.info("[视图主题拦截器]-清理主题.");
+        if (logger.isDebugEnabled()) {
+            logger.debug("[视图主题拦截器]-清理主题.");
         }
 
         WebViewThemeHolder.remove();
+    }
+
+    // ~~~~~~~~~~~~~~~~~~~~~ 依赖注入 ~~~~~~~~~~~~~~~~~~ //
+
+    public void setIgnoreUriPrefixs(Set<String> ignoreUriPrefixs) {
+        this.ignoreUriPrefixs = ignoreUriPrefixs;
     }
 
 }
